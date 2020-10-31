2cee8c4f4c6b38b56ff8a1fd83a615281fd67802
mawnir <62011430+mawnir@users.noreply.github.com>
2020-10-31 11:30:38 +0100

    Update navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -6,6 +6,7 @@ export default (props) => (
   <nav className="navigation"> 
     <Link to="/contact">Contact</Link>
     <Link to="/404">404</Link>
+    <a href='https://www.youtube.com/channel/UCXOgTJOW1n-PwPuRmOM8U6A?sub_confirmation=1' >Subscribe &rarr;</a>
     <ThemeChanger/>
   </nav>
   
