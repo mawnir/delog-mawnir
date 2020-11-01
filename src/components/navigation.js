00aed72cca9d49f9afc73c9acc9697d4251c04c1
mawnir <62011430+mawnir@users.noreply.github.com>
2020-11-01 15:55:39 +0100

    header add elements

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -7,6 +7,8 @@ export default (props) => (
     <Link to="/contact">Contact</Link>
     <Link to="/404">404</Link>
     <a href='https://www.youtube.com/channel/UCXOgTJOW1n-PwPuRmOM8U6A?sub_confirmation=1' >Subscribe &rarr;</a>
+    <Link to="/contact">Contact</Link>
+    <Link to="/404">404</Link>
     <ThemeChanger/>
   </nav>
   
