d954e590c30d49e92e61d602111bd4896ef29cdc
mawnir <62011430+mawnir@users.noreply.github.com>
2020-11-01 18:39:07 +0100

    menu icon

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,6 +1,7 @@
 import React from "react"
 import {Link} from "gatsby"
 import ThemeChanger from "../components/themeChanger"
+import Menu from "./menu"
 
 export default (props) => (
   <nav className="navigation"> 
@@ -8,8 +9,14 @@ export default (props) => (
     <Link to="/404">404</Link>
     <a href='https://www.youtube.com/channel/UCXOgTJOW1n-PwPuRmOM8U6A?sub_confirmation=1' >Subscribe &rarr;</a>
     <Link to="/contact">Contact</Link>
-    <Link to="/404">404</Link>
+    <Link to='/contact' className="button -primary">Get in &rarr;</Link>
+    <a to='/contact' class="icon" >
+      <i class="gg-menu-right" ></i>
+    </a>
     <ThemeChanger/>
   </nav>
-  
 )
+
+
+
+ 
