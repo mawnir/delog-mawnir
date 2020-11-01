82f61070745c653077476590af084d7853278142
mawnir <62011430+mawnir@users.noreply.github.com>
2020-11-01 18:48:07 +0100

    delete menu

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,7 +1,6 @@
 import React from "react"
 import {Link} from "gatsby"
 import ThemeChanger from "../components/themeChanger"
-import Menu from "./menu"
 
 export default (props) => (
   <nav className="navigation"> 
