5c8f32729e724dc8d93036e2f1f230a24396b28c
mawnir <62011430+mawnir@users.noreply.github.com>
2020-11-02 12:19:03 +0100

    remove menu

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,23 +1,35 @@
 import React from "react"
-import {Link} from "gatsby"
+import { Link } from "gatsby"
 import ThemeChanger from "../components/themeChanger"
-import Nav from "../components/menu"
 
 export default (props) => (
-  <nav className="navigation"> 
+  <nav className="navigation">
     <Link to="/contact">Contact</Link>
     <Link to="/404">404</Link>
     <a href='https://www.youtube.com/channel/UCXOgTJOW1n-PwPuRmOM8U6A?sub_confirmation=1' >Subscribe &rarr;</a>
     <Link to="/contact">Contact</Link>
-    <Link to='/contact' className="button -primary">Get in &rarr;</Link>
-    <a to='/contact' class="icon" >
-      <i class="gg-menu-right" ></i>
-    </a>
-    <ThemeChanger/>
-    <Nav/>
+
+    <div>
+
+      <div class="uk-inline ">
+      <span uk-icon="icon: menu; ratio: 1.3" class="uk-icon-link"></span>
+
+        <div uk-dropdown="mode: click;" class="uk-background-muted">
+          <ul className="uk-nav uk-dropdown-nav ">
+            <li class="uk-active"> <Link to="/contact">Contact</Link> </li>
+            <li > <Link to="/404"> <span class="uk-margin-small-right" uk-icon="icon: thumbnails"></span> 404</Link> </li>
+            <li class="uk-nav-divider"></li>
+            <li > <a href='https://www.youtube.com/channel/UCXOgTJOW1n-PwPuRmOM8U6A?sub_confirmation=1' >Subscribe &rarr;</a> </li>
+          </ul>
+        </div>
+      </div>
+
+
+    </div>
+
+    <ThemeChanger />
   </nav>
 )
 
 
 
- 
