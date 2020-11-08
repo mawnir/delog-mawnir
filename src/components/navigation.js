6c6131679733dfd0f550cb46c9551be0584f2dd9
mawnir <62011430+mawnir@users.noreply.github.com>
2020-11-08 10:20:33 +0100

    fix padding

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -7,25 +7,25 @@ import Icons from 'uikit/dist/js/uikit-icons';
 UIkit.use(Icons);
 export default (props) => (
   <nav className="navigation">
-    
+
     <Link to="/contact">Contact</Link>
     <Link to="/404">404</Link>
     <a href='https://www.youtube.com/channel/UCXOgTJOW1n-PwPuRmOM8U6A?sub_confirmation=1' >Subscribe &rarr;</a>
     <Link to="/contact">Contact</Link>
 
-    <div>
-      <div class="uk-inline ">
-        <i className="gg-menu-right-alt uk-icon-link"  ></i>
-        <div uk-dropdown="mode: click; pos: bottom-center" class="uk-background-muted">
-          <ul className="uk-nav uk-dropdown-nav">
-            <li class="uk-active"> <Link to="/contact">Contact</Link> </li>
-            <li > <Link to="/404"> <span class="uk-margin-small-right" uk-icon="icon: thumbnails"></span> 404</Link> </li>
-            <li class="uk-nav-divider"></li>
-            <li > <a href='https://www.youtube.com/channel/UCXOgTJOW1n-PwPuRmOM8U6A?sub_confirmation=1' >Subscribe &rarr;</a> </li>
-          </ul>
-        </div>
+
+    <div class="uk-inline ">
+      <i className="gg-menu-right-alt uk-icon-link"  ></i>
+      <div uk-dropdown="mode: click; pos: bottom-center" class="uk-background-muted">
+        <ul className="uk-nav uk-dropdown-nav">
+          <li class="uk-active"> <Link to="/contact">Contact</Link> </li>
+          <li > <Link to="/404"> <span class="uk-margin-small-right" uk-icon="icon: thumbnails"></span> 404</Link> </li>
+          <li class="uk-nav-divider"></li>
+          <li > <a href='https://www.youtube.com/channel/UCXOgTJOW1n-PwPuRmOM8U6A?sub_confirmation=1' >Subscribe &rarr;</a> </li>
+        </ul>
       </div>
     </div>
+
 
     <ThemeChanger />
   </nav>
