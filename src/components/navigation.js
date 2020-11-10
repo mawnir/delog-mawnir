49b4d53bb3adf53e5c294466fc3e8ec22e94bbc5
mawnir <62011430+mawnir@users.noreply.github.com>
2020-11-10 11:34:51 +0100

    new menu btn

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -13,20 +13,15 @@ export default (props) => (
     <a href='https://www.youtube.com/channel/UCXOgTJOW1n-PwPuRmOM8U6A?sub_confirmation=1' >Subscribe &rarr;</a>
     <Link to="/contact">Contact</Link>
 
-
-    <div class="uk-inline ">
-      <i className="gg-menu-right-alt uk-icon-link"  ></i>
-      <div uk-dropdown="mode: click; pos: bottom-center" class="uk-background-muted">
-        <ul className="uk-nav uk-dropdown-nav">
-          <li class="uk-active"> <Link to="/contact">Contact</Link> </li>
-          <li > <Link to="/404"> <span class="uk-margin-small-right" uk-icon="icon: thumbnails"></span> 404</Link> </li>
-          <li class="uk-nav-divider"></li>
-          <li > <a href='https://www.youtube.com/channel/UCXOgTJOW1n-PwPuRmOM8U6A?sub_confirmation=1' >Subscribe &rarr;</a> </li>
-        </ul>
+    <div class="dropdown" >
+      <i className="gg-menu-right-alt dropbtn"></i>
+      <div class="dropdown-content">
+        <Link to="/contact">Contact</Link>
+        <Link to="/404">404</Link>
+        <a href='https://www.youtube.com/channel/UCXOgTJOW1n-PwPuRmOM8U6A?sub_confirmation=1' >Subscribe &rarr;</a>
+        <Link to="/contact">Contact</Link>
       </div>
     </div>
-
-
     <ThemeChanger />
   </nav>
 )
