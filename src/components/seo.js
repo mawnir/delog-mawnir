f8b2a97e288e26c34214297575a9028ff0d7a505
mawnir <62011430+mawnir@users.noreply.github.com>
2020-11-11 18:42:32 +0100

    remove uikit

--- src/components/seo.js
+++ src/components/seo.js
@@ -11,21 +11,14 @@ const SEO = ({ seo = {} }) => {
         },
         {
           rel: "stylesheet",
-          href: "https://fonts.googleapis.com/css?family=Staatliches",
+          href: "",
         },
         
       ]}
       script={[
         {
           src:
-            "https://cdn.jsdelivr.net/npm/uikit@3.5.9/dist/js/uikit.min.js",
-        },
-        {
-          src:
-            "https://cdn.jsdelivr.net/npm/uikit@3.5.9/dist/js/uikit-icons.min.js",
-        },
-        {
-          src: "https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.js",
+            "",
         },
       ]}
     />
